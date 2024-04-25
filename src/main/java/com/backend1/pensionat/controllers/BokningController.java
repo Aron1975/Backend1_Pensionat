bd3874619f61e1ca077ed0d593d51644ba2706fb
Rickard Pettersson <rickard971@hotmail.com>
2024-04-25 18:01:18 +0200

    testing push

--- src/main/java/com/backend1/pensionat/controllers/BokningController.java
+++ src/main/java/com/backend1/pensionat/controllers/BokningController.java
@@ -25,6 +25,6 @@ public class BokningController {
         model.addAttribute("responseList", responseList);
         model.addAttribute("kat", "bokningar");
         model.addAttribute("titel", "Bokning");
-        return "/allaBokningar";  //testing
+        return "/allaBokningar";  //testing igen
     }
 }
