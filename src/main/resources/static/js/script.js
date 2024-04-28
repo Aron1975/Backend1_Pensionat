137a7cd475d3d9a231f9306b008e86408b0b1955
Aron Gyulai <arongyf@gmail.com>
2024-04-28 23:08:05 +0200

    start date chooser not show previous days

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,11 +2,20 @@ $(function () {
     $("#startdatepicker").datepicker({
         autoclose: true,
         todayHighlight: true,
-        minDate: '0'
+        startDate: new Date(),
     }).datepicker('update', new Date());
+    let stDate;
     $("#stopdatepicker").datepicker({
+        beforeShow: stDate = getStartDate(), //document.getElementById('startDate').value,
+        onselect: stDate = getStartDate(),
         autoclose: true,
         todayHighlight: true,
+        startDate: new Date(),//.setDate(stDate.getDate()+1),//new Date(stDate),//Date.parse(stDate)),
     }).datepicker('update', new Date());
 });
 
+   function getStartDate(){
+    var stDate = document.getElementById('startDate').value
+    console.log(stDate);
+    return stDate
+    }
