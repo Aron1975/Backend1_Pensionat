14ba4b147a8acf6f0f7132c48eeae3773276c21c
Pejman <pejman.elias@yh.nackademin.se>
2024-04-30 03:28:27 +0200

    updated regex patterns in firstname/lastname

--- src/main/java/com/backend1/pensionat/models/Kund.java
+++ src/main/java/com/backend1/pensionat/models/Kund.java
@@ -25,22 +25,30 @@ public class Kund {
     private long id;
 
     @NotBlank(message = "SSN number cannot be empty")
-    @Pattern(regexp="(^$|[0-9]{10,12})", message= "Mobile number must be between 10 and 12 digits")
+    @Pattern(regexp="(^$|[0-9]{10,12})", message= "SSN number must be between 10 and 12 digits")
     private String ssn;
+
     @NotBlank(message = "First name cannot be empty")
+    @Pattern(regexp = "(^$|^[A-Za-zåäöÅÄÖ]+$)", message = "First name must only contain letters")
     @Size(max = 50, message = "First name must be a maximum of 50 characters")
     private String förnamn;
+
     @NotBlank(message = "Last name cannot be empty")
+    @Pattern(regexp = "(^$|^[A-Za-zåäöÅÄÖ]+$)", message = "Last name must only contain letters")
     @Size(max = 50, message = "Last name must be a maximum of 50 characters")
     private String efternamn;
+
     @NotBlank(message = "Mobile number cannot be empty")
     @Pattern(regexp="(^$|[0-9]{10,12})", message= "Mobile number must be between 10 and 12 digits")
     private String mobilnummer;
+
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email should be valid")
     private String email;
+
     @NotBlank(message = "Address cannot be empty")
     private String adress;
+
     @NotBlank(message = "City cannot be empty")
     private String stad;
 
